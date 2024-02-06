efbd99e8b5589a3707a4350bf9ee6649902351fc
Henrique <henriquevigato@gmail.com>
2024-02-06 10:52:20 -0300

    Desafio interpreteador esolang parte 1

---
+++ esolang_interpreters_1.py
@@ -0,0 +1,14 @@
+def my_first_interpreter(code):
+    resposta = ""
+    number = 0
+    for letter in code:
+        if number > 255:
+            number = 0
+
+        if letter == "+":
+            number += 1
+        if letter == ".":
+            print(number)
+            resposta = resposta + chr(number)
+
+    return resposta
